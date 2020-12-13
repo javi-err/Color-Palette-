048e692c6bc91fd14dff1bf7ff2294276f6f8adf
javi <jsmart1993@hotmail.com>
2020-12-13 07:40:24 -0700

    created a copy overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,15 +2,37 @@ import React, { Component } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './ColorBox.css'
 export class ColorBox extends Component {
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            isCopied: false
+        }
+
+        this.handleCopy = this.handleCopy.bind(this)
+    }
+
+    
+    handleCopy(){
+        this.setState({isCopied: true}, () => {
+            setTimeout(() => this.setState({
+                isCopied: false
+            }), 1000)})
+        }
+    
+
     render() {
-        const {name, background} = this.props
+        const { name, background } = this.props
+        const { isCopied } = this.state
         return (
             <div className="ColorBox" style={{background}}>
+                <div className={`copy-overlay ${isCopied && 'copied'}`}><span>Copied!</span>{this.state.background}</div>
                 <div className="copy-container">
                     <div className="box-content">
                         <span>{name}</span>
                     </div>
-                <CopyToClipboard text={background}>
+                <CopyToClipboard text={background} onCopy={this.handleCopy}>
                     <button className="copy-button">Copy</button>
                 </CopyToClipboard>
                 </div>
